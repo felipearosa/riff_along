c0238d1b1bb135b0c191220cd534b91da3d0d82d
Felipe Arosa <felipe.arosa@gmail.com>
2022-08-29 16:12:07 -0300

    added user has many

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :lists
+  has_many :catalogs, through: :lists
+  has_many :videos, through: :catalogs
+  has_many :solos, through: :videos
 end
