df916d6c008d83a4794f4b7a9ce3b8a77f085d67
Felipe Arosa <felipe.arosa@gmail.com>
2022-08-31 14:55:38 -0300

    added js mark start seconds

--- app/javascript/controllers/video_controller.js
+++ app/javascript/controllers/video_controller.js
@@ -1,59 +1,72 @@
 import { Controller } from "@hotwired/stimulus"
+import YouTubePlayer from 'youtube-player';
 
 // Connects to data-controller="video"
 export default class extends Controller {
-
+  static targets = [ "start", "stop" ]
   static values = {
     slug: String
   }
 
 
   connect() {
-    this.done = false
-
-    console.log(this.slugValue);
-    this.onYouTubeIframeAPIReady()
+    console.log(this.startTarget);
+    this.done = false;
+    this.player = YouTubePlayer('player');
+    this.player.loadVideoById(this.slugValue);
+    this.player.playVideo();
   }
 
-
-
-
-  onPlayerStateChange = (event) => {
-    console.log('ok onPlayerStateChange')
-    if (event.data == YT.PlayerState.PLAYING && !this.done) {
-      //setTimeout(stopVideo, 6000);
-      //done = true;
-    }
-  }
-
-  stopVideo = () => {
-    console.log('ok stopVideo')
-    this.player.stopVideo();
-  }
-
-
-
-  onPlayerReady = (event) => {
-    console.log('ok onPlayerReady')
-    //event.target.playVideo();
-    event.target.loadVideoById(slug , 0.1, "large")
-  };
-
-  onYouTubeIframeAPIReady = () => {
-    console.log('ok onYouTubeIframeAPIReady')
-    this.player = new YT.Player('player', {
-      height: '390',
-      width: '640',
-      videoId: '',
-      playerVars: {
-        'playsinline': 1
-      },
-      events: {
-        'onReady': (event) => this.onPlayerReady(event),
-        'onStateChange': (event) => this.onPlayerStateChange(event)
-      }
+  markStart(){
+    console.log('working');
+    this.player.getCurrentTime()
+    .then(data => {
+      console.log(data)
     });
   }
+
+
+  // onPlayerStateChange = (event) => {
+  //   console.log('ok onPlayerStateChange')
+  //   if (event.data == YT.PlayerState.PLAYING && !this.done) {
+  //     //setTimeout(stopVideo, 6000);
+  //     //done = true;
+  //   }
+  // }
+
+  // stopVideo = () => {
+  //   console.log('ok stopVideo')
+  //   this.player.stopVideo();
+  // }
+
+
+
+  // onPlayerReady = (event) => {
+  //   console.log('ok onPlayerReady')
+  //   //event.target.playVideo();
+  //   event.target.loadVideoById(slug , 0.1, "large")
+  // };
+
+  // onYouTubeIframeAPIReady = () => {
+  //   console.log('ok onYouTubeIframeAPIReady')
+  //   this.player = YouTubePlayer('video', {
+  //     videoId: 'M7lc1UVf-VE'
+  //   });
+
+
+    // this.player = new YT.Player('player', {
+    //   height: '390',
+    //   width: '640',
+    //   videoId: '',
+    //   playerVars: {
+    //     'playsinline': 1
+    //   },
+    //   events: {
+    //     'onReady': (event) => this.onPlayerReady(event),
+    //     'onStateChange': (event) => this.onPlayerStateChange(event)
+    //   }
+    // });
+  //}
 
 
 }
