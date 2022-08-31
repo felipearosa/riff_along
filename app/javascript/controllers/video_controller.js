09b3105275cac0cbfbe61c2c72e66d0c09272607
Felipe Arosa <felipe.arosa@gmail.com>
2022-08-31 19:21:32 -0300

    created button setting time

--- app/javascript/controllers/video_controller.js
+++ app/javascript/controllers/video_controller.js
@@ -3,7 +3,7 @@ import YouTubePlayer from 'youtube-player';
 
 // Connects to data-controller="video"
 export default class extends Controller {
-  static targets = [ "start", "stop" ]
+  static targets = [ "start", "stop", "list", "row", "start", "end" ]
   static values = {
     slug: String
   }
@@ -15,22 +15,69 @@ export default class extends Controller {
     this.player = YouTubePlayer('player');
     this.player.loadVideoById(this.slugValue);
     this.player.playVideo();
+    console.log('sworking2');
   }
 
   markStart(){
-    console.log('working');
     this.player.getCurrentTime()
     .then(data => {
-      console.log(data)
+      this.exactStart = data
+      this.startTime = this.#turnSecIntoMin(data);
     });
   }
 
-  markStart(){
-    console.log('working');
+  markStop(){
     this.player.getCurrentTime()
     .then(data => {
-      console.log(data)
+      this.exactEnd = data
+      this.endTime = this.#turnSecIntoMin(data);
+      const row = document.createElement('tr');
+      row.innerHTML = `
+                <td>(mastered_img)</td>
+                <td>${this.startTime} - ${this.endTime}</td>
+                <td class="btn btn-primary" data-video-target="row"  data-video-start="${this.exactStart}" data-video-end="${this.exactEnd}" data-action="click->video#playSolo">Go to Solo</td>
+
+            `;
+      this.listTarget.appendChild(row);
     });
+  }
+
+  //
+
+
+  playSolo(event){
+    const element = event.target
+    console.log(element.dataset.videoStart);
+    this.player.loadVideoById({'videoId': this.slugValue,
+    'startSeconds': element.dataset.videoStart,
+    'endSeconds': element.dataset.videoEnd,
+    'suggestedQuality': 'large'});
+  }
+
+  #turnSecIntoMin(seconds){
+    let startTime
+    if(seconds <60){
+      const exactSecond = Math.floor(seconds)
+      if(exactSecond <10){
+        startTime = `0:0${exactSecond}`
+
+      } else {
+        startTime = `0:${exactSecond}`
+      }
+    } else {
+      const startTimeMin = Math.floor(seconds/60)
+      const startTimeSec = Math.floor(seconds%60)
+
+      if(startTimeSec <10){
+        startTime = `${startTimeMin}:0${startTimeSec}`
+
+      } else {
+        startTime = `${startTimeMin}:${startTimeSec}`
+      }
+
+    }
+
+    return startTime
   }
 
 
