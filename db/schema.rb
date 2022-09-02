dc6fb4576cc2d725f2a739d0a816175cd7fe8ae1
Eduardo <eduardotps@gmail.com>
2022-09-02 11:49:18 -0300

    created migrations, seeds and some small front stuffs

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_29_205623) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_02_141545) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -76,6 +76,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_29_205623) do
     t.string "youtube_key"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "title"
+    t.string "image_url"
   end
 
   add_foreign_key "catalogs", "lists"
--- db/seeds.rb
+++ db/seeds.rb
@@ -78,17 +78,29 @@ List.create(name: 'Indie', description: 'Not emo, indie. Just indie. And hate.',
 List.create(name: 'Brega', description: 'List to feed my soul and grow mullets.', user_id: 10)
 List.create(name: 'Solos', description: 'My favorite solos songs', user_id: 10)
 
-Video.create(youtube_key: '20Ov0cDPZy8')
-Video.create(youtube_key: 'obkrMiyDrbs')
-Video.create(youtube_key: 'iK8mD_qA3lQ')
-Video.create(youtube_key: 'bpZvg_FjL3Q')
-Video.create(youtube_key: 'kHjzuqq3b44')
-Video.create(youtube_key: 'MMFj8uDubsE')
-Video.create(youtube_key: 'qFfnlYbFEiE')
-Video.create(youtube_key: '6EOK8gPsGzo')
-Video.create(youtube_key: 'iaE4s3m8UOQ')
-Video.create(youtube_key: 'rdi7G1hY4-w')
+Video.create(youtube_key: '20Ov0cDPZy8', title: "John Mayer - Free Fallin' (Live at the Nokia Theatre)", image_url: 'https://i.ytimg.com/vi/20Ov0cDPZy8/hqdefault.jpg')
+Video.create(youtube_key: 'obkrMiyDrbs', title: "Alicia Keys & John Mayer - If I ain't got you - Gravity (Better audio quality)", image_url: 'https://i.ytimg.com/vi/obkrMiyDrbs/hqdefault.jpg')
+Video.create(youtube_key: 'iK8mD_qA3lQ', title: "John Mayer Greatest Hits -John Mayer Full Album 2021", image_url: 'https://i.ytimg.com/vi/iK8mD_qA3lQ/hqdefault.jpg')
+Video.create(youtube_key: 'bpZvg_FjL3Q', title: "Bob Dylan - Hurricane (Official Audio)", image_url: 'https://i.ytimg.com/vi/bpZvg_FjL3Q/hqdefault.jpg')
+Video.create(youtube_key: 'kHjzuqq3b44', title: "Bob Dylan Greatest Hits - Best Songs of Bob Dylan (HQ)", image_url: 'https://i.ytimg.com/vi/kHjzuqq3b44/hqdefault.jpg')
+Video.create(youtube_key: 'MMFj8uDubsE', title: "Bob Dylan - Blowin' in the Wind (Official Audio)", image_url: 'https://i.ytimg.com/vi/MMFj8uDubsE/hqdefault.jpg')
+Video.create(youtube_key: 'qFfnlYbFEiE', title: "The Jimi Hendrix Experience - Voodoo Child (Slight Return) (Live In Maui, 1970)", image_url: 'https://i.ytimg.com/vi/qFfnlYbFEiE/hqdefault.jpg')
+Video.create(youtube_key: '6EOK8gPsGzo', title: "Jimi Hendrix greatest hits (full album) - Best songs of Jimi Hendrix {COOL MUSIC}", image_url: 'https://i.ytimg.com/vi/6EOK8gPsGzo/hqdefault.jpg')
+Video.create(youtube_key: 'iaE4s3m8UOQ', title: "The Jimi Hendrix Experience - Little Wing [Legendado/Tradução]", image_url: 'https://i.ytimg.com/vi/iaE4s3m8UOQ/hqdefault.jpg')
+Video.create(youtube_key: 'rdi7G1hY4-w', title: "Its My Life | Sri Lankan Version | Sandaru Sathsara", image_url: 'https://i.ytimg.com/vi/rdi7G1hY4-w/hqdefault.jpg')
 
-Catalog.create()
+50.times do |i|
+  i += 1
+  Catalog.create(list_id: i, video_id: 1)
+  Catalog.create(list_id: i, video_id: 2)
+  Catalog.create(list_id: i, video_id: 3)
+  Catalog.create(list_id: i, video_id: 4)
+  Catalog.create(list_id: i, video_id: 5)
+  Catalog.create(list_id: i, video_id: 6)
+  Catalog.create(list_id: i, video_id: 7)
+  Catalog.create(list_id: i, video_id: 8)
+  Catalog.create(list_id: i, video_id: 9)
+  Catalog.create(list_id: i, video_id: 10)
+end
 
 puts "Seeding done. Finally."
