86a1591b4eab76aa0d71b12cbfc2e792abdeed2c
Eduardo <eduardotps@gmail.com>
2022-09-05 15:23:51 -0300

    inserted last 5 and show all

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -55,7 +55,7 @@ class ListsController < ApplicationController
   def destroy
     @list.destroy
 
-    redirect_to lists_path, status: :see_other
+    redirect_to user_path(current_user), status: :see_other
   end
 
   private
