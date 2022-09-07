16c524e83a22843b72626efcba1a6fc2854999d5
Felipe Arosa <felipe.arosa@gmail.com>
2022-09-07 13:56:17 -0300

    fixed user show view-all

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  skip_before_action :authenticate_user!, only: %i[show]
   def show
     @user = User.find(params[:id])
     @achievements = Achievement.all
