e45b7219a3143ea4e45f11445982940b4eaf6604
Felipe <felipe.arosa@gmail.com>
2022-10-05 11:29:26 -0300

    added riff to solos table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_06_145426) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_05_141604) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -56,6 +56,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_06_145426) do
     t.bigint "video_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "riff"
     t.index ["video_id"], name: "index_solos_on_video_id"
   end
 
