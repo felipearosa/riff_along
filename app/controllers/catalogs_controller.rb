245b3ab75b1fdc0ddf0197b76363ba5e2063b726
Felipe <felipe.arosa@gmail.com>
2022-10-13 17:12:38 -0300

    test

--- app/controllers/catalogs_controller.rb
+++ app/controllers/catalogs_controller.rb
@@ -7,6 +7,7 @@ class CatalogsController < ApplicationController
   end
 
   def index
+    @catalogs = Catalog.all
   end
 
   def create
@@ -15,11 +16,7 @@ class CatalogsController < ApplicationController
     unless @video
       video_id = CGI.escape(params[:catalog][:video_id])
       url = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet&id=#{video_id}&key=#{ENV.fetch('YOUTUBE_API')}"
-<<<<<<< HEAD
-      videos_serialized = URI.open(url, 'User-Agent' => 'Ruby').read
-=======
       videos_serialized = RestClient.get url
->>>>>>> a4e8b46e6f8bdff2b37cb668140bf46945bce875
       videos_info = JSON.parse(videos_serialized)["items"][0]
       video_title = videos_info["snippet"]["title"]
       video_image_url = videos_info["snippet"]["thumbnails"]["medium"]["url"]
