839df88adb98dbd37a72c47e4970d93f609bad67
Felipe <felipe.arosa@gmail.com>
2022-10-13 16:08:50 -0300

    updated open uri

--- app/controllers/catalogs_controller.rb
+++ app/controllers/catalogs_controller.rb
@@ -15,7 +15,7 @@ class CatalogsController < ApplicationController
     unless @video
       video_id = CGI.escape(params[:catalog][:video_id])
       url = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet&id=#{video_id}&key=#{ENV.fetch('YOUTUBE_API')}"
-      videos_serialized = URI.open(url).read
+      videos_serialized = URI.open(url,'User-Agent' => 'ruby').read
       videos_info = JSON.parse(videos_serialized)["items"][0]
       video_title = videos_info["snippet"]["title"]
       video_image_url = videos_info["snippet"]["thumbnails"]["medium"]["url"]
